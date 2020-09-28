a760383d8566d86156be3467b8f51059009ea70f
VelGros <71959947+VelGros@users.noreply.github.com>
2020-09-28 08:17:19 +0200

    v3
    Major rendering change: lines are now split into chunks, not words anymore.
    This fix the issue with , and . colored at end of lines
    This also allow users to tag a whole sentence with * rather than every words in it.
    
    Also added 3 templates.
    You may notice the dirty trick for fontsize with different templates.
    I could do better using JavaScript but i don't want to waste my time learning JS for such a small thing.
    If you have JS skills, feel free to submit a commit request with improvements :)

--- img.php
+++ img.php
@@ -1,36 +1,71 @@
 <?php
 	header("Content-Type: image/jpeg");
 	
-	#Check input values against XSS attacks.
+	//Check input values against XSS attacks.
 	if((!IsSet($_GET['text'])) OR (strlen($_GET['text']) > 500)) // Limit text lenght to 500 chars to avoid high load.
-		{	$_GET['text'] = "Votre texte *ici*"; }
-	if((!IsSet($_GET['fontSize'])) OR ($_GET['fontSize'] < 50) OR ($_GET['fontSize'] > 200))
+		{	$_GET['text'] = "Votre texte ici "; }
+	if((!IsSet($_GET['fontSize'])) OR ($_GET['fontSize'] < 10) OR ($_GET['fontSize'] > 200))
 		{	$_GET['fontSize'] = 70;}
+	
+	
+	// Default values
+	$def_x = 150;
+	$y = 600;
+	
+	// Select template
+	define("IMGFOLDER","./src/templates/");
+	switch($_GET['template'])
+	{
+		case "incompetent":
+			$imgPath = IMGFOLDER."incompetent.jpg";
+			break;
+		case "mini":
+			$imgPath = IMGFOLDER."mini.jpg";
+			$def_x = 50;
+			$y = 150;
+			break;	
+		default:
+			$imgPath = IMGFOLDER."default.jpg";
+			break;	
+	}
 		
-	$img = imagecreatefromjpeg("./src/template.jpg"); 
+	$img = imagecreatefromjpeg($imgPath); 
 	$font = realpath("./src/nowblack.ttf");
 	
 	$fontSize = $_GET['fontSize']; 
 	
-	$x = 150;
-	$y = 600;
-	$string = mb_strtoupper(urldecode($_GET['text']));;
+
+	$string = mb_strtoupper(urldecode($_GET['text']));
 	$lines = explode("\r\n",$string);
 	
+	$x = $def_x;
 	foreach ($lines as $line)
 	{
-		$words = explode(' ',$line);
-		foreach($words as $word)
+		/* Chunks are delimited by starting and ending * symbol
+		Anything than doesn't contains * symbol is also a chunk.
+		
+		For instance:
+		"Pour mettre un *texte en jaune*, entourez le *d'asterisques*"
+		
+		Chunks are:
+		1) "Pour mettre un "
+		2) "*texte en jaune*"
+		3) ", entourez le "
+		4) *d'asterisques* 		*/
+		
+		$chunks = preg_split("#(\*.+\*)#",$line,-1,PREG_SPLIT_DELIM_CAPTURE);
+		foreach($chunks as $chunk)
 		{
-			if(preg_match("#(\*.+\*)#",$word))// Word is tagged with * (eg: "*yellow*")
+			if(preg_match("#(\*.+\*)#",$chunk))// Chunk is tagged with * (eg: "*yellow*")
 				{	$color = ImageColorAllocate($img, 254, 224, 2);		} //Yellow
 			else
 				{	$color = ImageColorAllocate($img, 255, 255, 255);	} //White
-			$pos = imagettftext($img,$fontSize,0,$x,$y,$color,$font,str_replace('*','',$word));
+			$pos = imagettftext($img,$fontSize,0,$x,$y,$color,$font,str_replace('*','',$chunk));
 			$x = $pos[2]+$fontSize/2.5;
 		}
-		$x = 150;
-		$y = $pos[3]+$fontSize+50;
+		
+		$x = $def_x;
+		$y = $pos[3]+$fontSize*1.8;
 	}
 	
 	Imagejpeg($img);
--- index.php
+++ index.php
@@ -13,26 +13,47 @@
 		Malgré quelques bons messages, la plupart des affiches versent dans le #VictimBlaming et la déresponsabilisation des conducteurs de véhicules motorisés.
 		<br/><br/>
 		Pour souligner le ridicule de cette campagne tout en riant un bon coup, détournons la !	
+		#SécuritéRoutiéreLOL Tweet comme @RoutePlusSur
 	<p>
 	
 	<h2> La machine à détourner </h2>
 		<?php // Define default values
 			if(!IsSet($_POST['text']))
-				{	$_POST['text'] = "Votre texte ici \r\nPour mettre un mot en *jaune*, \r\nentourez le du symbole astérisque."; }
+				{	$_POST['text'] = "Votre texte ici \r\nPour mettre un *texte en jaune*, \r\nentourez le du symbole astérisque."; }
+			if(!IsSet($_POST['template']))
+				{	$_POST['template'] = "default";	}
 			if(!IsSet($_POST['fontSize']))
 				{	$_POST['fontSize'] = 70;}
+			if($_POST['fontSize'] == 70 AND $_POST['template'] == "mini")
+				{	$_POST['fontSize'] = 20; }
+			if($_POST['fontSize'] == 20 AND $_POST['template'] != "mini")
+				{	$_POST['fontSize'] = 70; }
+
 		?>
 		
-		<img src="img.php<?php echo("?text=".urlencode($_POST['text'])."&fontSize=".$_POST['fontSize']);?>" id="preview"/> <br/>
+		<img src="img.php<?php echo("?text=".urlencode($_POST['text'])."&fontSize=".$_POST['fontSize']."&template=".$_POST['template']);?>" id="preview"/> <br/>
 		
 		<form action="index.php" method="post" class="main">
 			<textarea id="text" name="text" rows="3" ><?php echo $_POST['text']; ?></textarea><br/>
-			<span id="fontSize">Taille du texte: <input type="number" name="fontSize" value="<?php echo $_POST['fontSize']?>" min="50" max="200" > </span>
+			<span id="fontSize">Taille du texte: <input type="number" name="fontSize" value="<?php echo($_POST['fontSize']); ?>" min="10" max="200" > </span>
+			
+			Modéle:
+			<input type="radio" id="template_default" name="template" value="default" <?php if($_POST['template'] == "default") {echo 'checked="true"';} ?> >
+			<label for="template_default">Parodie (défaut)</label>
+			<input type="radio" id="template_com" name="template" value="incompetent" <?php if($_POST['template'] == "incompetent") {echo 'checked="true"';} ?> >
+			<label for="template_com">Incompétent</label>
+			<input type="radio" id="template_mini" name="template" value="mini" <?php if($_POST['template'] == "mini") {echo 'checked="true"';} ?> >
+			<label for="template_mini">Mini</label>
+
 			<input type="submit" value="Valider" id="submit">
 		</form>
+		
 	</p>
 	<p>
 		<br/><br/>
+		<a rel="noopener noreferrer" target="_blank" href="https://twitter.com/intent/tweet?text=Toi%20aussi%20tweet%20comme%20@RoutePlusSur%20avec&hashtags=securiteroutierelol&url=http://insecurite-routiere.lived.fr">
+		<img src="./src/twitter.png"/></a>
+		<br/>
 		Crée par <a href="https://twitter.com/VelGros">VelGros</a>.
 		Code source: <a href="https://github.com/VelGros/secu.routiere-detournement">GitHub repo</a>
 	</p>

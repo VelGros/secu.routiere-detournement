ba8509b51aaf09a64750974d068918a3fb817036
VelGros <71959947+VelGros@users.noreply.github.com>
2021-05-13 18:07:32 +0200

    May 2021 - Text update
    Added a sentence about new French Road Safety office campaign + credits to @garde_boue for the originale idea.

--- index.php
+++ index.php
@@ -9,10 +9,11 @@
 	<h1> Détournons la campagne de la sécurité routière </h1>
 	
 	<p class="main">
-		Le 24 septembre 2020, la sécurité routière a lancé une vaste campagne de sensibilisation. 
-		Malgré quelques bons messages, la plupart des affiches versent dans le #VictimBlaming et la déresponsabilisation des conducteurs de véhicules motorisés.
+		Le 24 septembre 2020, la sécurité routière a lancée une vaste campagne de sensibilisation. 
+		Malgré quelques bons messages, la plupart des affiches versent dans le #VictimBlaming et la déresponsabilisation des conducteurs de véhicules motorisés.		
+		Cette campagne a été reprise sans correction en Mai 2021 sous le nom Mai à vélo.
 		<br/><br/>
-		Pour souligner le ridicule de cette campagne tout en riant un bon coup, détournons-la !	
+		Pour souligner le ridicule de cette campagne tout en riant un bon coup, détournons la !	
 		#SécuritéRoutiéreLOL Tweet comme @RoutePlusSur
 	<p>
 	
@@ -22,21 +23,11 @@
 				{	$_POST['text'] = "Votre texte ici \r\nPour mettre un *texte en jaune*, \r\nentourez le du symbole astérisque."; }
 			if(!IsSet($_POST['template']))
 				{	$_POST['template'] = "default";	}
-				
-			/* DEFAULT FONTSIZES
-			Change is made directly into the POST var.
-			To avoid overwriting user settings, defaults values are detected by values.
-			Yep, this means you cannot user large template default value (70) in mini ones and vice-versa.
-			
-			This could be easily fixed using some JS to just change default value on click on radio buttons
-			but i don't want to learn JS nor use some script i don't understand for that 
-			so feel free to enchance :)  */
-			
 			if(!IsSet($_POST['fontSize']))
-				{	$_POST['fontSize'] = 70;}// Default fontSize
-			if($_POST['fontSize'] == 70 AND $_POST['template'] == "mini")// If user select mini template but still has d efault fontsize.
-				{	$_POST['fontSize'] = 20; }//Default fontsize for mini template
-			if($_POST['fontSize'] == 20 AND $_POST['template'] != "mini")// If user select a large tempalte but still has  mini default fontSize
+				{	$_POST['fontSize'] = 70;}
+			if($_POST['fontSize'] == 70 AND $_POST['template'] == "mini")
+				{	$_POST['fontSize'] = 20; }
+			if($_POST['fontSize'] == 20 AND $_POST['template'] != "mini")
 				{	$_POST['fontSize'] = 70; }
 
 		?>
@@ -47,7 +38,7 @@
 			<textarea id="text" name="text" rows="3" ><?php echo $_POST['text']; ?></textarea><br/>
 			<span id="fontSize">Taille du texte: <input type="number" name="fontSize" value="<?php echo($_POST['fontSize']); ?>" min="10" max="200" > </span>
 			
-			Modèle:
+			Modéle:
 			<input type="radio" id="template_default" name="template" value="default" <?php if($_POST['template'] == "default") {echo 'checked="true"';} ?> >
 			<label for="template_default">Parodie (défaut)</label>
 			<input type="radio" id="template_com" name="template" value="incompetent" <?php if($_POST['template'] == "incompetent") {echo 'checked="true"';} ?> >
@@ -61,11 +52,13 @@
 	</p>
 	<p>
 		<br/><br/>
-		<a rel="noopener noreferrer" target="_blank" href="https://twitter.com/intent/tweet?text=Toi%20aussi%20tweet%20comme%20@RoutePlusSur%20avec&hashtags=securiteroutierelol&url=http://insecurite-routiere.lived.fr">
+		<a rel="noopener noreferrer" target="_blank" href="https://twitter.com/intent/tweet?text=Toi%20aussi%20tweet%20comme%20@RoutePlusSur%20avec&hashtags=securiteroutierelol&url=https://insecurite-routiere.fr/">
 		<img src="./src/twitter.png"/></a>
 		<br/>
-		Créé par <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/VelGros">VelGros</a>.
-		Code source: <a rel="noopener noreferrer" target="_blank" href="https://github.com/VelGros/secu.routiere-detournement">GitHub repo</a>
+		Crée par <a href="https://twitter.com/VelGros">VelGros</a>.
+		Code source: <a href="https://github.com/VelGros/secu.routiere-detournement">GitHub repo</a>
+		<br/>
+		Sur une idée originale de <a href="https://twitter.com/garde_boue">@garde_boue</a>.
 	</p>
 	</body>
 </html>
